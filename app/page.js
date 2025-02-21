1f5288d249d13a6e510f43f4febf8a0ffca0d5e6
John Doe <johndoe@example.com>
2025-02-21 12:27:44 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+
+export default function Home() {
+  return (
+    <ClerkProvider>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </ClerkProvider>
+  );
+}
